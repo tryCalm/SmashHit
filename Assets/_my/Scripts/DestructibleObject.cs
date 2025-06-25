bd0ff1f85477c586009e8b77c5b623ffb1885398
tryCalm <artemzhirnov3@gmail.com>
2025-06-25 21:13:56 +0300

    feat: destructible objects behaviour

---
+++ Assets/_my/Scripts/DestructibleObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class DestructibleObject : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+        StartCoroutine(DestroyCoroutine());
+    }
+    
+    public IEnumerator DestroyCoroutine()
+    {
+        yield return new WaitForSeconds(5);
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+    }
+}
